a4e9bba7840fab907c42af71d0dd5d39b10436bb
Bear <995748222@qq.com>
2020-03-19 21:51:23 +0800

    自定义异常并捕获

--- src/main/java/com/bear/start/controller/ExceptionController.java
+++ src/main/java/com/bear/start/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.bear.start.controller;
 
+import com.bear.start.domain.MyException;
 import com.bear.start.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,10 @@ public class ExceptionController {
     public Object exceptionTest(){
         int a = 1/0;
         return new User("lxx","abc",18,"ssss",new Date(),"111");
+    }
+
+    @GetMapping("/myException")
+    public Object MyException(){
+        throw new MyException("400","myExption");
     }
 }
--- src/main/java/com/bear/start/domain/exception/GetException.java
+++ src/main/java/com/bear/start/domain/exception/GetException.java
@@ -1,5 +1,6 @@
 package com.bear.start.domain.exception;
 
+import com.bear.start.domain.MyException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,12 +10,19 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GetException {
-
+    private Map<String,Object> map = new HashMap<>();
     @ExceptionHandler(value =Exception.class)
     public Map<String, Object> Get(Exception e, HttpServletRequest request){
-        Map<String,Object> map = new HashMap<>();
         map.put("code",100);
         map.put("message",e.getMessage());
+        map.put("url",request.getRequestURL());
+        return map;
+    }
+
+    @ExceptionHandler(value = MyException.class)
+    public Map MyException(MyException e, HttpServletRequest request){
+        map.put("code",e.getCode());
+        map.put("message",e.getMsg());
         map.put("url",request.getRequestURL());
         return map;
     }
