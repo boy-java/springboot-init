87db10f1b26d4c36bea5706a13403e4211385742
Bear <995748222@qq.com>
2020-03-24 21:15:06 +0800

    自定义原生servlet解释

--- src/main/java/com/bear/start/controller/UserServlet.java
+++ src/main/java/com/bear/start/controller/UserServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * servlet相当于现在的get/post...请求 在doget中得到请求然后可以对请求进行处理 然后发到tomocat中  在dopost中
+ * 将请求放行
+ */
 @WebServlet(name = "UserServlet",urlPatterns = {"/d"})
 public class UserServlet extends HttpServlet {
     @Override
